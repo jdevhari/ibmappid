b61b88dbfb8e1929a97aab4cabed9cff28222fc0
jdevhari <jdev.hari@gmail.com>
2019-04-04 13:13:07 -0700

    cleanup

--- src/main/java/com/mstsolutions/test/Security/SecurityApplication.java
+++ src/main/java/com/mstsolutions/test/Security/SecurityApplication.java
@@ -22,21 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jayway.jsonpath.JsonPath;
 
 @SpringBootApplication
-@EnableOAuth2Sso //Enables OAuth2 Single Sign On, will automatically use application.yml properties for security
+@EnableOAuth2Sso
 @EnableAutoConfiguration
-@RestController //Enabling REST functionality. With this, we can now expose our own endpoints
-public class SecurityApplication extends WebSecurityConfigurerAdapter  {
+@RestController
+public class SecurityApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		HashMap<String, Object> props = new HashMap<>();
-		
+
 		try {
 			String vcapServices = System.getenv().get("VCAP_SERVICES");
 			Optional<Map<String, Object>> maybeCredentials = parseOAuth2Credentials(vcapServices);
-			System.out.println(maybeCredentials.orElseThrow(() -> new RuntimeException("Oauth2 credentials not found in VCAP_SERVICES")));
+			System.out.println(maybeCredentials
+					.orElseThrow(() -> new RuntimeException("Oauth2 credentials not found in VCAP_SERVICES")));
 
 			Map<String, Object> credentials = maybeCredentials.get();
-			
 
 			props.put("security.oauth2.client.clientId", credentials.get("clientId"));
 			props.put("security.oauth2.client.clientSecret", credentials.get("clientSecret"));
@@ -48,59 +48,53 @@ public class SecurityApplication extends WebSecurityConfigurerAdapter  {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//SpringApplication.run(SecurityApplication.class, args);
-		new SpringApplicationBuilder()
-        .sources(SecurityApplication.class)
-        .properties(props)
-        .run(args);
+		new SpringApplicationBuilder().sources(SecurityApplication.class).properties(props).run(args);
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	public static Optional<Map<String, Object>> parseOAuth2Credentials(String vcapServices) {
-        if (vcapServices != null) {
-            List<Map<String, Object>> services = JsonPath.parse(vcapServices)
-                    .read("$.*.[?(@.credentials)]", List.class);
+		if (vcapServices != null) {
+			List<Map<String, Object>> services = JsonPath.parse(vcapServices).read("$.*.[?(@.credentials)]",
+					List.class);
+			return services.stream().filter(o -> {
+				Collection<String> tags = (Collection<String>) o.get("tags");
+				Collection<String> credentialKeys = ((Map<String, Object>) o.get("credentials")).keySet();
+				return (tags != null && tags.contains("oauth2")) || credentialKeys.contains("authorizationEndpoint");
+			}).findFirst().map(t -> (Map<String, Object>) t.get("credentials"));
 
-            return services.stream().filter(o -> {
-                Collection<String> tags = (Collection<String>) o.get("tags");
-                Collection<String> credentialKeys =  ((Map<String, Object>) o.get("credentials")).keySet();
-                return (tags != null && tags.contains("oauth2")) || credentialKeys.contains("authorizationEndpoint");
-            }).findFirst().map(t -> (Map<String, Object>)t.get("credentials"));
+		}
+		return Optional.empty();
+	}
 
-        }
-        return Optional.empty();
-    }
-	
 	@Override
-    protected void configure(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception {
+		// Configuring Spring security access. For /login, /user, and /userinfo, we need
+		// authentication.
+		// Logout is enabled.
+		// Adding csrf token support to this configurer.
+		http.authorizeRequests().antMatchers("/login**", "/user", "/userInfo").authenticated().antMatchers("/test**")
+				.permitAll().and().logout().logoutSuccessUrl("/").permitAll().and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
 
-        //Configuring Spring security access. For /login, /user, and /userinfo, we need authentication.
-        //Logout is enabled.
-        //Adding csrf token support to this configurer.
-        http.authorizeRequests()
-                .antMatchers("/login**", "/user","/userInfo").authenticated()
-                .antMatchers("/test**").permitAll()
-                .and().logout().logoutSuccessUrl("/").permitAll()
-                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-
-    }
-	
 	@RequestMapping("/test")
-    public String printPros() {
-        return "";
-    }
-	
-	@RequestMapping("/user")
-    public Principal user(Principal principal) {
-        //Principal holds the logged in user information.
-        // Spring automatically populates this principal object after login.
-        return principal;
-    }
+	public String printPros() {
+		return "";
+	}
 
-    @RequestMapping("/userInfo")
-    public String userInfo(Principal principal){
-        final OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
-        final Authentication authentication = oAuth2Authentication.getUserAuthentication();
-        //Manually getting the details from the authentication, and returning them as String.
-        return authentication.getDetails().toString();
-    }
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		// Principal holds the logged in user information.
+		// Spring automatically populates this principal object after login.
+		return principal;
+	}
+
+	@RequestMapping("/userInfo")
+	public String userInfo(Principal principal) {
+		final OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
+		final Authentication authentication = oAuth2Authentication.getUserAuthentication();
+		// Manually getting the details from the authentication, and returning them as
+		// String.
+		return authentication.getDetails().toString();
+	}
 }
